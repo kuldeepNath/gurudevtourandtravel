c588e2111e50a5c8e5189a2ed9ac3dfaaaadba2b
kuldeep~ <kuldeepnath96@gmail.com~>
2024-04-26 19:59:21 +0530

    first commit

---
+++ src/app/components/share-details/share-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+@Component({
+    selector: 'app-share-details',
+    templateUrl: './share-details.component.html',
+    styleUrls: ['./share-details.component.scss'],
+    styles: [`
+    ngb-progressbar {
+        margin-top: 5rem;
+    }
+    `]
+})
+
+export class ShareDetailsComponent implements OnInit {
+    name = new FormControl('', Validators.required);
+    mobile = new FormControl('', Validators.required);
+    email = new FormControl('', Validators.required);
+    desciption = new FormControl('', Validators.required);
+    ngOnInit() { 
+        
+    }
+    sentToWhatsapp() {
+        debugger
+        let content="Name : "+this.name.value+" \n "+"Mobile No : "+this.mobile.value+"\n "+"EmailId : "+this.email.value
+        +" \n "+"Trip Details : "+this.desciption.value;
+        let html = document.createElement('div');
+        html.innerHTML = content;       
+        let link = "https://api.whatsapp.com/send?phone="+this.mobile.value+"&text="+html.innerHTML;
+        window.open(link, "_blank");
+    }
+}
